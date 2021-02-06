6d43ef1d03d50870f77a88705b47313cf4316f8f
DivyaAnandaraja <77230591+DivyaAnandaraja@users.noreply.github.com>
2021-02-06 09:49:14 +0530

    Create Pro1.java

---
+++ Pro1.java
@@ -0,0 +1,21 @@
+package zoho1;
+import java.util.*;
+
+public class Pro1 {
+	public static void main(String args[]) {
+		int n=5;
+	for(int i=n;i>=1;i--) {
+		for(int j=5;j>=1;j--) {
+			if(i>=j) {
+				System.out.print(i+" ");
+			}
+			else
+			{
+				System.out.print(j+" ");
+			}
+		}
+		System.out.println();
+	}
+	}
+}
+
